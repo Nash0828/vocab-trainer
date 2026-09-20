3ef3e07121d6c0b11f19c9c19ae54b4e136f48ec
Nash0828 <Nash0828@users.noreply.github.com>
2026-09-21 01:57:27 +0800

    fix: 切换页面时自动滚动到顶部

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import InputView from '../views/InputView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior() {
+    return { top: 0 }
+  },
   routes: [
     {
       path: '/',
